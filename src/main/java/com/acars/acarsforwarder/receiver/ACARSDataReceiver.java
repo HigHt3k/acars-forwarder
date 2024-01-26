21b86c35241351a9587c78013a574bbc77d172f6
Johann Töpfer <johann.toepfer@lht.dlh.de>
2024-01-26 10:31:48 +0100

    implemented reading new data in batches

--- src/main/java/com/acars/acarsforwarder/receiver/ACARSDataReceiver.java
+++ src/main/java/com/acars/acarsforwarder/receiver/ACARSDataReceiver.java
@@ -1,28 +1,34 @@
 package com.acars.acarsforwarder.receiver;
 
-import jakarta.annotation.PostConstruct;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Component
 public class ACARSDataReceiver {
 
     private long sentMessages = 0;
+    private long lastMessageIdSent = 0;
 
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
     private final Logger logger = LoggerFactory.getLogger(ACARSDataReceiver.class);
 
-    public ACARSDataReceiver(RestTemplate restTemplate) {
+    public ACARSDataReceiver(RestTemplate restTemplate, ObjectMapper objectMapper) {
         this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
     }
 
-    @PostConstruct
+    @Scheduled(fixedRate = 5000)
     public void retrieveACARSData() {
         logger.info("Scheduled data send job");
         sendData();
@@ -30,17 +36,7 @@ public class ACARSDataReceiver {
 
     private void sendData() {
         selectCount();
-
-        /*
-
-        String endpointUrl = "https://34.28.80.116:1880/acars";
-        String requestData = "";
-        logger.info("SENDING DATA: {}", requestData);
-        sentMessages++;
-        logger.info("Total amount of sent messages: {}", sentMessages);
-        restTemplate.put(endpointUrl, requestData);
-
-         */
+        sendNextMessages();
     }
 
     private Connection connect() {
@@ -57,6 +53,43 @@ public class ACARSDataReceiver {
         return conn;
     }
 
+    private void sendNextMessages() {
+        String endpointUrl = "https://34.28.80.116:1880/acars";
+
+        String sql = "SELECT * FROM Messages WHERE message_id > " + lastMessageIdSent;
+
+        try(Connection conn = this.connect()) {
+            logger.info("Excecuting query to get data...");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            logger.info("Found result for messages with id > {}", lastMessageIdSent);
+            while(rs.next()) {
+                AcarsData acarsData = new AcarsData();
+                acarsData.setMessageId(rs.getInt(1));
+                lastMessageIdSent = acarsData.getMessageId();
+                acarsData.setFlightId(rs.getInt(2));
+                acarsData.setTime(LocalDateTime.now());
+                acarsData.setStId(rs.getInt(4));
+                acarsData.setChannel(rs.getInt(5));
+                acarsData.setError(rs.getInt(6));
+                acarsData.setSignalLvl(rs.getInt(7));
+                acarsData.setMode(rs.getString(8));
+                acarsData.setAck(rs.getString(9));
+                acarsData.setLabel(rs.getString(10));
+                acarsData.setBlockNo(rs.getString(11));
+                acarsData.setMessNo(rs.getString(12));
+                acarsData.setText(rs.getString(13));
+
+                String requestData = convertToJson(acarsData);
+
+                logger.info("[Total MSG: {}] - SENDING DATA: {}", sentMessages, requestData);
+                restTemplate.put(endpointUrl, requestData);
+            }
+        } catch(SQLException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
     private void selectCount() {
         String sql = "SELECT count(*) FROM Messages";
 
@@ -69,6 +102,16 @@ public class ACARSDataReceiver {
             }
         } catch(SQLException e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    private String convertToJson(AcarsData data) {
+        try {
+            return objectMapper.writeValueAsString(data);
+        } catch (Exception e) {
+            // Handle or log the exception as needed
+            e.printStackTrace();
+            return null;
         }
     }
 }
---
+++ src/main/java/com/acars/acarsforwarder/receiver/AcarsData.java
@@ -0,0 +1,27 @@
+package com.acars.acarsforwarder.receiver;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class AcarsData {
+    int messageId;
+    int flightId;
+    LocalDateTime time;
+    int stId;
+    int channel;
+    int error;
+    int signalLvl;
+    String mode;
+    String ack;
+    String label;
+    String blockNo;
+    String messNo;
+    String text;
+}
