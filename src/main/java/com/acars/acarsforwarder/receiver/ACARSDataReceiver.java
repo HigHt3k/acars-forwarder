52e8d03292de93b31a3fc36854764b71844140ba
Johann Töpfer <johann.toepfer@lht.dlh.de>
2024-01-26 10:06:26 +0100

    added logging

--- src/main/java/com/acars/acarsforwarder/receiver/ACARSDataReceiver.java
+++ src/main/java/com/acars/acarsforwarder/receiver/ACARSDataReceiver.java
@@ -1,5 +1,6 @@
 package com.acars.acarsforwarder.receiver;
 
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,8 +14,6 @@ public class ACARSDataReceiver {
 
     private long sentMessages = 0;
 
-    private final String pathToFile = "/home/pi/acarsserv/acarsserv.sqb";
-
     private final RestTemplate restTemplate;
 
     private final Logger logger = LoggerFactory.getLogger(ACARSDataReceiver.class);
@@ -23,36 +22,37 @@ public class ACARSDataReceiver {
         this.restTemplate = restTemplate;
     }
 
-    @Scheduled(fixedRate = 5000)
+    @PostConstruct
     public void retrieveACARSData() {
         logger.info("Scheduled data send job");
         sendData();
     }
 
     private void sendData() {
-        String endpointUrl = "https://34.28.80.116:1880/acars";
-
-        String requestData = "";
-
         selectCount();
 
+        /*
+
+        String endpointUrl = "https://34.28.80.116:1880/acars";
+        String requestData = "";
         logger.info("SENDING DATA: {}", requestData);
         sentMessages++;
         logger.info("Total amount of sent messages: {}", sentMessages);
-
         restTemplate.put(endpointUrl, requestData);
+
+         */
     }
 
     private Connection connect() {
         Connection conn = null;
         try {
-            String url = "jdbc:sqlite:" + pathToFile;
+            String url = "jdbc:sqlite:/home/pi/acarsserv/acarsserv.sqb";
             conn = DriverManager.getConnection(url);
 
             logger.info("Connection established");
 
         } catch(SQLException e) {
-            logger.error(e.getMessage());
+            logger.error("Connection to file couldn't be established: {}", e.getMessage());
         }
         return conn;
     }
